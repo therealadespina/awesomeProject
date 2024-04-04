e2031c70901d36bf256803476c96321665814ccb
k.sergey <k.sergey>
2024-04-04 21:41:01 +0300

    add application.yaml

--- cmd/repository/dataSource.go
+++ cmd/repository/dataSource.go
@@ -4,10 +4,40 @@ import (
 	"context"
 	"fmt"
 	"github.com/jackc/pgx/v4"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 )
 
+type ApplicationConfig struct {
+	AwesomeProject struct {
+		DataSource string `yaml:"dataSourcePath"`
+	} `yaml:"awesomeProject"`
+}
+
 func DataSource() (*Application, error) {
-	conn, err := pgx.Connect(context.Background(), "postgresql://test:test@localhost:5432/test")
+	// Чтение файла application.yaml
+	yamlFile, err := ioutil.ReadFile("application.yaml")
+	if err != nil {
+		log.Fatalf("Ошибка чтения файла application.yaml: %v", err)
+	}
+
+	// Создание экземпляра структуры для хранения конфигурации
+	var appConfig ApplicationConfig
+
+	// Разбор YAML данных
+	err = yaml.Unmarshal(yamlFile, &appConfig)
+	if err != nil {
+		log.Fatalf("Ошибка разбора YAML: %v", err)
+	}
+
+	// Использование извлеченных переменных
+	fmt.Println("DataSourcePath is:", appConfig.AwesomeProject.DataSource)
+
+	if err != nil {
+		log.Fatalf("Ошибка чтения файла YAML: %v", err)
+	}
+	conn, err := pgx.Connect(context.Background(), appConfig.AwesomeProject.DataSource)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
--- go.mod
+++ go.mod
@@ -21,5 +21,6 @@ require (
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/gorm v1.25.9 // indirect
 )
